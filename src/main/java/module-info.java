82e0c32285f0cf75400bc90d41343f7d004726eb
Artyom Belsky <artyombel2003@gmail.com>
2021-10-03 17:50:07 +0300

    Sonarcloud integrations in build.yml and module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,7 +10,6 @@ module com.example.financecontrol {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires java.sql;
-    requires org.xerial.sqlitejdbc;
 
     opens com.example.financecontrol to javafx.fxml;
     exports com.example.financecontrol;
