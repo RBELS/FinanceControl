9527ab9ed5b3d8f9d83033f7557d8cff4b53c308
Dana <dano4ka1007@gmail.com>
2021-10-21 12:39:31 +0300

    changes in validationUtil, check in negative price input

--- src/main/java/com/example/financecontrol/utils/ValidationUtil.java
+++ src/main/java/com/example/financecontrol/utils/ValidationUtil.java
@@ -2,6 +2,16 @@ package com.example.financecontrol.utils;
 
 import java.util.logging.Logger;
 
+class NegativeNumberException extends Exception
+{
+    public NegativeNumberException() {}
+
+    public NegativeNumberException(String message)
+    {
+        super(message);
+    }
+}
+
 public class ValidationUtil {
     private static final Logger logger = Logger.getLogger(ValidationUtil.class.getName());
 
@@ -11,6 +21,19 @@ public class ValidationUtil {
             Double.parseDouble(str);
         }
         catch (NumberFormatException e) {
+            logger.info(e.toString());
+            return false;
+        }
+        try
+        {
+            Double price = Double.parseDouble(str);
+            if(price <= 0)
+            {
+                throw new NegativeNumberException();
+            }
+        }
+        catch(NegativeNumberException e)
+        {
             logger.info(e.toString());
             return false;
         }
