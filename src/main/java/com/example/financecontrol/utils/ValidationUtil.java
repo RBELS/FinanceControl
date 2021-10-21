55216a7a2b293250dfa01cb6db6fa64033cd0b62
Artyom Belsky <artyombel2003@gmail.com>
2021-10-21 20:56:38 +0300

    Changed order in bottom tab, fixed validation in ValidationUtil

--- src/main/java/com/example/financecontrol/utils/ValidationUtil.java
+++ src/main/java/com/example/financecontrol/utils/ValidationUtil.java
@@ -2,38 +2,18 @@ package com.example.financecontrol.utils;
 
 import java.util.logging.Logger;
 
-class NegativeNumberException extends Exception
-{
-    public NegativeNumberException() {}
-
-    public NegativeNumberException(String message)
-    {
-        super(message);
-    }
-}
-
 public class ValidationUtil {
     private static final Logger logger = Logger.getLogger(ValidationUtil.class.getName());
 
-    private static boolean isNumeric(String str) {
+    private static boolean validatePrice(String str) {
         if (str == null) return  false;
         try {
-            Double.parseDouble(str);
-        }
-        catch (NumberFormatException e) {
-            logger.info(e.toString());
-            return false;
-        }
-        try
-        {
-            Double price = Double.parseDouble(str);
-            if(price <= 0)
-            {
-                throw new NegativeNumberException();
+            double price = Double.parseDouble(str);
+            if(price <= 0) {
+                return false;
             }
         }
-        catch(NegativeNumberException e)
-        {
+        catch (NumberFormatException e) {
             logger.info(e.toString());
             return false;
         }
@@ -43,8 +23,6 @@ public class ValidationUtil {
     public static boolean validateInputEI(String categoryValue, String nameTextFieldValue, String priceTextFieldValue) {
         return categoryValue != null
                 && !nameTextFieldValue.equals("")
-                && isNumeric(priceTextFieldValue);
+                && validatePrice(priceTextFieldValue);
     }
-
-
 }
