b5fd545fb3ff7f70a5f51dcec6d9aa5f48f2880c
Dana <dano4ka1007@gmail.com>
2021-11-11 21:41:39 +0300

    generating a javadoc to the FinanceControlApplication, Main, DBController and ControllerFinals classes

--- src/main/java/com/example/financecontrol/FinanceControlApplication.java
+++ src/main/java/com/example/financecontrol/FinanceControlApplication.java
@@ -7,7 +7,17 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * FinanceControlApplication class which extends an Application class and from which the main view of the program launches
+ * @author Dana
+ * @version 1.0
+ */
 public class FinanceControlApplication extends Application {
+    /**
+     * start method which sets and shows the main view of the program
+     * @param stage the main Stage object is passed to the start () method, when the application starts, this object defines the main window or screen of the program
+     * @throws IOException when the I/O operations were failed or interrupted
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(FinanceControlApplication.class.getResource("main-view.fxml"));
@@ -19,6 +29,10 @@ public class FinanceControlApplication extends Application {
         stage.show();
     }
 
+    /**
+     * main () method from which Java program execution starts
+     * @param args command line arguments as an array of String objects
+     */
     public static void main(String[] args) {
         launch();
     }
